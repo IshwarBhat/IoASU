9d4c195d17701300321f5ee02f16532c19e34935
draja2011 <draja2011@gmail.com>
2017-04-22 13:57:40 -0700

    Add files via upload

--- IoASU.sql
+++ IoASU.sql
@@ -1,8 +1,10 @@
 /********************************************************
-* This script creates the database named my_guitar_shop 
+* This script creates the database named IoASU
 *********************************************************/
 USE master;
 GO
+
+-- alter database IoASU set single_user with rollback immediate;
 
 IF  DB_ID('IoASU') IS NOT NULL
 DROP DATABASE IoASU;
@@ -44,7 +46,6 @@ CREATE TABLE Departments (
   Department        VARCHAR(255)
 );
 
-
 CREATE TABLE Users (
   UserID            INT            PRIMARY KEY   IDENTITY,
   ASUID             VARCHAR(20),
@@ -52,9 +53,8 @@ CREATE TABLE Users (
   PasswordSalt      VARCHAR(128),
   LName             VARCHAR(255) NOT NULL,
   FName             VARCHAR(255) NOT NULL,
-  Bio               VARCHAR(255),
-  Email             VARCHAR(255) NOT NULL
-  );
+  Bio               VARCHAR(255)
+    );
 
 CREATE TABLE Phones (
   Phone             VARCHAR(10)    PRIMARY KEY
@@ -198,17 +198,17 @@ SET IDENTITY_INSERT Departments OFF;
 SET IDENTITY_INSERT Users ON;
 
 
-INSERT INTO Users (UserID, ASUID, Password, PasswordSalt, LName, FName, Bio, Email) VALUES
-(1, 'pbuffet', 'January01@', NULL, 'Buffet', 'Pheobe', 'Student of Information Technology', 'pbuffet@asu.edu'),
-(2, 'kclarkson', 'February8', NULL, 'Clarkson', 'Kelly', 'Civil Engineering Student', 'kclarkson@asu,edu'),
-(3, 'bpitt', 'March123@', NULL, 'Pitt', 'Brad', 'Biomedical Student', 'bpitt@asu.edu'),
-(4, 'jlawrence', 'April09%', NULL, 'Tribianni', 'Joseph', 'Biodesign Student', 'jlawrence@asu.edu'),
-(5, 'jtribianni', 'June123@', NULL, 'Buffet', 'Pheobe', 'Computer Science Student', 'jtribianni@asu.edu'),
-(6, 'cbing', 'Flower##', NULL, 'Bing', 'Chandler', 'Graphic Information technology Student', 'cbing@asu.edu'),
-(7, 'rgeller', 'September9@', NULL, 'Geller', 'Ross', 'Law Student', 'rgeller@asu.edu'),
-(8, 'rgreen', 'August!120', NULL, 'Green', 'Rachel', 'Accounting Student', 'rgreen@asu.edu'),
-(9, 'mgeller', 'December%2', NULL, 'Geller', 'Monica', 'Software Engineering Student', 'mgeller@asu.edu'),
-(10,'omunn', 'october763@', NULL, 'Olivia', 'Munn', 'Real Estate Student', 'omunn@asu.edu');
+INSERT INTO Users (UserID, ASUID, Password, PasswordSalt, LName, FName, Bio) VALUES
+(1, 'pbuffet', 'January01@', NULL, 'Buffet', 'Pheobe', 'Student of Information Technology'),
+(2, 'kclarkson', 'February8', NULL, 'Clarkson', 'Kelly', 'Civil Engineering Student'),
+(3, 'bpitt', 'March123@', NULL, 'Pitt', 'Brad', 'Biomedical Student'),
+(4, 'jlawrence', 'April09%', NULL, 'Tribianni', 'Joseph', 'Biodesign Student'),
+(5, 'jtribianni', 'June123@', NULL, 'Buffet', 'Pheobe', 'Computer Science Student'),
+(6, 'cbing', 'Flower##', NULL, 'Bing', 'Chandler', 'Graphic Information technology Student'),
+(7, 'rgeller', 'September9@', NULL, 'Geller', 'Ross', 'Law Student'),
+(8, 'rgreen', 'August!120', NULL, 'Green', 'Rachel', 'Accounting Student'),
+(9, 'mgeller', 'December%2', NULL, 'Geller', 'Monica', 'Software Engineering Student'),
+(10,'omunn', 'october763@', NULL, 'Olivia', 'Munn', 'Real Estate Student');
 
 
 SET IDENTITY_INSERT Users OFF;
@@ -403,4 +403,3 @@ ON *
 TO MGSUser@localhost
 IDENTIFIED BY 'pa55word';
 */
-
