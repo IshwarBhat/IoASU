de31482eaa2243307baf33b1f514229b1bda30f0
draja2011 <draja2011@gmail.com>
2017-04-23 13:38:56 -0700

    Add files via upload

---
+++ scripts/DCL.sql
@@ -0,0 +1,13 @@
+USE IoASU;
+
+CREATE LOGIN SuperAdmin WITH PASSWORD = '8SuNy<76G,', 
+DEFAULT_DATABASE = IoASU,
+CHECK_EXPIRATION =ON,
+CHECK_POLICY =ON;
+
+CREATE USER SuperAdmin;
+
+-- SuperAdmin can perform any activity on IoASU Database
+GRANT ALTER ON DATABASE::IoASU
+TO SuperAdmin;
+ 
