3a6407958e30b4ea6e046dc325af3e9011bc5670
ISHWAR BHAT <ishwrbhat@gmail.com>
2017-04-29 19:02:18 -0700

    Another stored procedures file

---
+++ scripts/otherProcedures.sql
@@ -0,0 +1,15 @@
+-- Stored Procedure: Uploading Documents
+-- Given OrgID, DocName, DocDesc
+-- Insert into Documents and DocumentOrganization tables
+GO
+CREATE PROC sp_DocumentUpload
+@OrgID INT,
+@DocName varchar(255),
+@DocDesc varchar(255)
+AS
+BEGIN
+DECLARE @ScopeIdentity INT;
+INSERT INTO Documents (DocName, DocDesc, DocLink) VALUES (@DocName,@DocDesc, '');
+SELECT @ScopeIdentity=SCOPE_IDENTITY();
+INSERT INTO DocumentOrganization (DocID, OrgID) VALUES (@ScopeIdentity, @OrgID);
+END
