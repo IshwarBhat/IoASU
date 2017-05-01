2b5e0bd23bf67b96da5d732c0581b1b89b13e403
ISHWAR BHAT <ishwrbhat@gmail.com>
2017-05-01 01:51:00 -0700

    Added new user JustAnotherUser using DCL

--- scripts/DCL.sql
+++ scripts/DCL.sql
@@ -11,3 +11,16 @@ CREATE USER SuperAdmin;
 GRANT ALTER ON DATABASE::IoASU
 TO SuperAdmin;
  
+ ---------------------------------------------------------------------------
+-- General User, who uses the mobile application.
+-- This user has permissions to view, insert and update tables in dbo schema.
+CREATE LOGIN JustAnotherUser WITH PASSWORD = '<AK31r-B8C',
+DEFAULT_DATABASE = IoASU,
+CHECK_EXPIRATION =ON,
+CHECK_POLICY =ON;
+
+CREATE USER JustAnotherUser;
+
+GRANT SELECT, UPDATE, INSERT
+ON SCHEMA::dbo
+TO JustAnotherUser;
